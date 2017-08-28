7faa6d0eddeef522030f858a3d4dccafabca0e36
kmcc063 <kmcc063@aucklanduni.ac.nz>
2017-08-28 20:08:06 +1200

    Further Updates
    Questions answered

--- HelloUnity_Final/Assets/Sketch.cs
+++ HelloUnity_Final/Assets/Sketch.cs
@@ -3,28 +3,48 @@ using System.Collections;
 
 public class Sketch : MonoBehaviour {
 
+    public GameObject SpherePreFab;
     public GameObject myPrefab;
+    
+    
 
     // Use this for initialization
     void Start() { 
     
         int totalCubes = 30;
         float totalDistance = 2.9f;
-             
+
         //sin distro
         for (int i = 0; i < totalCubes; i++)
         {
-            float perc = i / (float)totalCubes;
-            float sin = Mathf.Sin(perc * Mathf.PI/2);
+            if (i % 2 == 0){
+                float perc = i / (float)totalCubes;
+                float sin = Mathf.Sin(perc * Mathf.PI / 2);
 
-            float x = 1.8f + sin * totalDistance;
-            float y = 5.0f;
-            float z = 0.0f;
-            
-            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+                float x = 1.8f + sin * totalDistance;
+                float y = 5.0f;
+                float z = 0.0f;
 
-            newCube.GetComponent<CubeScript>().SetSize(.45f * (1.0f - perc));
-            newCube.GetComponent<CubeScript>().rotateSpeed = .2f + perc*4.0f;
+                var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+
+                newCube.GetComponent<CubeScript>().SetSize(.45f * (1.0f - perc));
+                newCube.GetComponent<CubeScript>().rotateSpeed = .2f + perc * 4.0f;
+
+            } else {
+                float perc = i / (float)totalCubes;
+                float sin = Mathf.Sin(perc * Mathf.PI / 2);
+
+                float x = 1.8f + sin * totalDistance;
+                float y = 5.0f;
+                float z = 0.0f;
+
+                var newSphere = (GameObject)Instantiate(SpherePreFab, new Vector3(x, y, z), Quaternion.identity);
+
+                newSphere.GetComponent<SphereCode>().SetSize(.45f * (1.0f - perc));
+                newSphere.GetComponent<SphereCode>().rotateSpeed = .2f + perc * 4.0f;
+
+            }
+
         }
     }
 	
